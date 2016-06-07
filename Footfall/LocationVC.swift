1d046e54209dabd1067f723e880ae931bac72c34
jackhider <jack@redih.co.uk>
2016-06-07 15:15:10 +0100

    added splash screen logo added base layout on map view

--- Footfall/LocationVC.swift
+++ Footfall/LocationVC.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import MapKit
 
-class LocationVC: UIViewController {
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
 
+    @IBOutlet weak var tableView:UITableView!
+    @IBOutlet weak var map:MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
 
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            
+            map.showsUserLocation = true
+            
+        }else {
+            
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        
+    }
 }
 
